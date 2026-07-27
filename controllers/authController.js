4af5a0012c0b4a50e0615215b750f061e3febf2e
Neeraj Saini <neerajboy756@gmail.com>
2026-07-27 21:25:29 +0530

    Fix: Resend 403 - use TEST_EMAIL for unverified domains

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,7 +18,7 @@ const sendEmail = async (to, subject, html) => {
   const resend = new Resend(process.env.RESEND_API_KEY);
   await resend.emails.send({
     from: 'BookShare <onboarding@resend.dev>',
-    to,
+    to: process.env.NODE_ENV === 'production' ? to : process.env.TEST_EMAIL || to,
     subject,
     html
   });
